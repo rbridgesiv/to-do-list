5a90594c5784fe4ae5c69aed887c41cd035e32f8
Robert Bridges <robertbridges@Roberts-MacBook-Pro-2.local>
2025-09-24 14:39:15 +0300

    make new file for adding projects

---
+++ src/addProject.js
@@ -0,0 +1,11 @@
+export function loadAddProject() {
+    const main = document.getElementById('main');
+
+    const formContainer = document.createElement('div');
+    formContainer.classList = 'formContainer';
+    main.appendChild(formContainer);
+
+    const form = document.createElement('form');
+    form.setAttribute('action, ')
+
+}
