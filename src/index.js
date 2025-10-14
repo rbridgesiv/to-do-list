9b4a6d8d1cf5acc7b135be3c8868e720b2bc8f7c
Robert Bridges <robertbridges@Roberts-MacBook-Pro-2.local>
2025-10-14 12:28:18 +0300

    add changes to save form input to localStorage

--- src/index.js
+++ src/index.js
@@ -1,8 +1,12 @@
 import './styles.css';
 import { loadAddProject } from './addProject.js';
+import { createProjectButton, loadProjectsFromStorage } from './projectStorage.js';
 
 const addProjectBtn = document.getElementById('addProject');
+const menu = document.getElementById('menu');
+
+loadProjectsFromStorage(menu);
 
 addProjectBtn.addEventListener ('click', () => {
-    loadAddProject();
+    loadAddProject(menu);
 })
